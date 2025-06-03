08719103aee9e420fd0e5d389a895d4e451c0a08
cballevre <celestin.ballevre@protonmail.com>
2025-06-03 16:36:18 +0200

    feat: Migrate ChakraProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,19 @@ import React, { useState } from 'react';
 import TideForm from './components/TideForm/TideForm';
 import { Tide } from './models/tide';
 import TideDisplay from './components/TideDisplay/TideDisplay';
-import { ChakraProvider, Container, Heading, Text } from '@chakra-ui/react';
+import { ChakraProvider, Container, Heading, Text , defaultSystem } from '@chakra-ui/react';
+
+
 
 const App: React.FC = () => {
   const [tide, setTide] = useState<Tide | null>(null);
   return (
-    <ChakraProvider>
+    <ChakraProvider value={defaultSystem}>
       <Container maxWidth="lg" mt={10}>
         <Heading as="h1" mb={1} size="xl" color="teal.500">
           Calculateur de marée
         </Heading>
-        <Text mb={6} size="lg">
+        <Text mb={6} fontSize="lg">
           Selon la règle des douzièmes
         </Text>
         <TideForm onSubmit={(data) => setTide(data)}></TideForm>
