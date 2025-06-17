daca4b0871ba730bf7b79126a0a001667b3563cc
cballevre <celestin.ballevre@protonmail.com>
2025-06-17 10:25:27 +0200

    feat: Generate pwa icon with new logo

--- vite.config.ts
+++ vite.config.ts
@@ -13,22 +13,40 @@ export default defineConfig({
       registerType: 'autoUpdate',
       injectRegister: false,
 
-      pwaAssets: {
-        disabled: false,
-        config: true,
-      },
-
       manifest: {
-        short_name: "Marée 12ème",
-        name: "Marée 12ème - Calculateur de marée selon la règle des douzièmes",
+        short_name: 'Marée 12ème',
+        name: 'Marée 12ème - Calculateur de marée selon la règle des douzièmes',
         theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
       },
 
       workbox: {
         globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
         cleanupOutdatedCaches: true,
         clientsClaim: true,
-      }
+      },
     }),
   ],
   server: {
