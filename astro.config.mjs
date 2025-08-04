4e5a8c87d2a35862a6a5706b980ca69d472057fa
cballevre <celestin.ballevre@protonmail.com>
2025-08-04 16:50:16 +0200

    feat: Use relative path to fix deploy on GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://cballevre.github.io/maree12eme/',
   integrations: [
     react(),
     AstroPWA({
